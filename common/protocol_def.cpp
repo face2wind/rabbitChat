84ac02af2754327ecd9639b1380812ec71a350e0
face2wind <dream.liuguobiao@gmail.com>
2016-11-03 14:10:08 +0800

    change message protocol

--- common/protocol_def.cpp
+++ common/protocol_def.cpp
@@ -6,8 +6,13 @@ __CSRegisterAccountDescribe__ for_describe_register_to___csregisteraccountdescri
 __CSLoginDescribe__ for_describe_register_to___cslogindescribe__;
 __SCLoginResultDescribe__ for_describe_register_to___scloginresultdescribe__;
 __CSRequestFriendListDescribe__ for_describe_register_to___csrequestfriendlistdescribe__;
-__FriendItemDescribe__ for_describe_register_to___frienditemdescribe__;
+__UserItemDescribe__ for_describe_register_to___useritemdescribe__;
 __SCFriendListDescribe__ for_describe_register_to___scfriendlistdescribe__;
+__CSRequestAllUserListDescribe__ for_describe_register_to___csrequestalluserlistdescribe__;
+__SCAllUserListDescribe__ for_describe_register_to___scalluserlistdescribe__;
+__CSRequestMakeFriendDescribe__ for_describe_register_to___csrequestmakefrienddescribe__;
+__SCMakefriendRequestListDescribe__ for_describe_register_to___scmakefriendrequestlistdescribe__;
+__CSChatToUserDescribe__ for_describe_register_to___cschattouserdescribe__;
 
 
 void CSRegisterAccount::Serialize(ByteArray &collector) const
@@ -52,15 +57,15 @@ void CSRequestFriendList::Unserialize(ByteArray &collector)
 {
 }
 
-void FriendItem::Serialize(ByteArray &collector) const
+void UserItem::Serialize(ByteArray &collector) const
 {
-  collector.WriteUint32(player_id);
+  collector.WriteUint32(id);
   collector.WriteString(name);
 }
 
-void FriendItem::Unserialize(ByteArray &collector)
+void UserItem::Unserialize(ByteArray &collector)
 {
-  player_id = collector.ReadUint32();
+  id = collector.ReadUint32();
   name = collector.ReadString();
 }
 
@@ -77,13 +82,87 @@ void SCFriendList::Unserialize(ByteArray &collector)
 {
   {
     int array_size = collector.ReadUint16();
-    FriendItem tmp_attr_value;
+    UserItem tmp_attr_value;
     for (int index = 0; index < array_size; ++ index)
     {
       tmp_attr_value.Unserialize(collector);
       friend_list.push_back(tmp_attr_value);
     }
   }
+}
+
+void CSRequestAllUserList::Serialize(ByteArray &collector) const
+{
+}
+
+void CSRequestAllUserList::Unserialize(ByteArray &collector)
+{
+}
+
+void SCAllUserList::Serialize(ByteArray &collector) const
+{
+  collector.WriteUint16((unsigned short)user_list.size());
+  for (auto array_item : user_list)
+  {
+    array_item.Serialize(collector);
+  }
+}
+
+void SCAllUserList::Unserialize(ByteArray &collector)
+{
+  {
+    int array_size = collector.ReadUint16();
+    UserItem tmp_attr_value;
+    for (int index = 0; index < array_size; ++ index)
+    {
+      tmp_attr_value.Unserialize(collector);
+      user_list.push_back(tmp_attr_value);
+    }
+  }
+}
+
+void CSRequestMakeFriend::Serialize(ByteArray &collector) const
+{
+  collector.WriteUint32(user_id);
+}
+
+void CSRequestMakeFriend::Unserialize(ByteArray &collector)
+{
+  user_id = collector.ReadUint32();
+}
+
+void SCMakefriendRequestList::Serialize(ByteArray &collector) const
+{
+  collector.WriteUint16((unsigned short)user_list.size());
+  for (auto array_item : user_list)
+  {
+    array_item.Serialize(collector);
+  }
+}
+
+void SCMakefriendRequestList::Unserialize(ByteArray &collector)
+{
+  {
+    int array_size = collector.ReadUint16();
+    UserItem tmp_attr_value;
+    for (int index = 0; index < array_size; ++ index)
+    {
+      tmp_attr_value.Unserialize(collector);
+      user_list.push_back(tmp_attr_value);
+    }
+  }
+}
+
+void CSChatToUser::Serialize(ByteArray &collector) const
+{
+  collector.WriteUint32(user_id);
+  collector.WriteString(chat_message);
+}
+
+void CSChatToUser::Unserialize(ByteArray &collector)
+{
+  user_id = collector.ReadUint32();
+  chat_message = collector.ReadString();
 }
 
 }
--- common/protocol_def.hpp
+++ common/protocol_def.hpp
@@ -51,25 +51,75 @@ public:
   virtual const std::string GetTypeName() const { return "CSRequestFriendList"; }
 };
 
-class FriendItem : public SerializeBase
+class UserItem : public SerializeBase
 {
 public:
-  unsigned int player_id;
+  unsigned int id;
   std::string name;
 
   virtual void Serialize(ByteArray &collector) const;
   virtual void Unserialize(ByteArray &collector);
-  virtual const std::string GetTypeName() const { return "FriendItem"; }
+  virtual const std::string GetTypeName() const { return "UserItem"; }
 };
 
 class SCFriendList : public SerializeBase
 {
 public:
-  std::vector<FriendItem> friend_list;
+  std::vector<UserItem> friend_list;
 
   virtual void Serialize(ByteArray &collector) const;
   virtual void Unserialize(ByteArray &collector);
   virtual const std::string GetTypeName() const { return "SCFriendList"; }
+};
+
+class CSRequestAllUserList : public SerializeBase
+{
+public:
+
+  virtual void Serialize(ByteArray &collector) const;
+  virtual void Unserialize(ByteArray &collector);
+  virtual const std::string GetTypeName() const { return "CSRequestAllUserList"; }
+};
+
+class SCAllUserList : public SerializeBase
+{
+public:
+  std::vector<UserItem> user_list;
+
+  virtual void Serialize(ByteArray &collector) const;
+  virtual void Unserialize(ByteArray &collector);
+  virtual const std::string GetTypeName() const { return "SCAllUserList"; }
+};
+
+class CSRequestMakeFriend : public SerializeBase
+{
+public:
+  unsigned int user_id;
+
+  virtual void Serialize(ByteArray &collector) const;
+  virtual void Unserialize(ByteArray &collector);
+  virtual const std::string GetTypeName() const { return "CSRequestMakeFriend"; }
+};
+
+class SCMakefriendRequestList : public SerializeBase
+{
+public:
+  std::vector<UserItem> user_list;
+
+  virtual void Serialize(ByteArray &collector) const;
+  virtual void Unserialize(ByteArray &collector);
+  virtual const std::string GetTypeName() const { return "SCMakefriendRequestList"; }
+};
+
+class CSChatToUser : public SerializeBase
+{
+public:
+  unsigned int user_id;
+  std::string chat_message;
+
+  virtual void Serialize(ByteArray &collector) const;
+  virtual void Unserialize(ByteArray &collector);
+  virtual const std::string GetTypeName() const { return "CSChatToUser"; }
 };
 
 class __CSRegisterAccountDescribe__ : public SerializeDescribe
@@ -112,14 +162,14 @@ protected:
   virtual SerializeBase * CreateSerialize() const { return new CSRequestFriendList(); }
 };
 
-class __FriendItemDescribe__ : public SerializeDescribe
+class __UserItemDescribe__ : public SerializeDescribe
 {
 public:
-  __FriendItemDescribe__() { GetNameToObjectMap()["FriendItem"] = this; }
-  virtual ~__FriendItemDescribe__() {}
+  __UserItemDescribe__() { GetNameToObjectMap()["UserItem"] = this; }
+  virtual ~__UserItemDescribe__() {}
 
 protected:
-  virtual SerializeBase * CreateSerialize() const { return new FriendItem(); }
+  virtual SerializeBase * CreateSerialize() const { return new UserItem(); }
 };
 
 class __SCFriendListDescribe__ : public SerializeDescribe
@@ -130,6 +180,56 @@ public:
 
 protected:
   virtual SerializeBase * CreateSerialize() const { return new SCFriendList(); }
+};
+
+class __CSRequestAllUserListDescribe__ : public SerializeDescribe
+{
+public:
+  __CSRequestAllUserListDescribe__() { GetNameToObjectMap()["CSRequestAllUserList"] = this; }
+  virtual ~__CSRequestAllUserListDescribe__() {}
+
+protected:
+  virtual SerializeBase * CreateSerialize() const { return new CSRequestAllUserList(); }
+};
+
+class __SCAllUserListDescribe__ : public SerializeDescribe
+{
+public:
+  __SCAllUserListDescribe__() { GetNameToObjectMap()["SCAllUserList"] = this; }
+  virtual ~__SCAllUserListDescribe__() {}
+
+protected:
+  virtual SerializeBase * CreateSerialize() const { return new SCAllUserList(); }
+};
+
+class __CSRequestMakeFriendDescribe__ : public SerializeDescribe
+{
+public:
+  __CSRequestMakeFriendDescribe__() { GetNameToObjectMap()["CSRequestMakeFriend"] = this; }
+  virtual ~__CSRequestMakeFriendDescribe__() {}
+
+protected:
+  virtual SerializeBase * CreateSerialize() const { return new CSRequestMakeFriend(); }
+};
+
+class __SCMakefriendRequestListDescribe__ : public SerializeDescribe
+{
+public:
+  __SCMakefriendRequestListDescribe__() { GetNameToObjectMap()["SCMakefriendRequestList"] = this; }
+  virtual ~__SCMakefriendRequestListDescribe__() {}
+
+protected:
+  virtual SerializeBase * CreateSerialize() const { return new SCMakefriendRequestList(); }
+};
+
+class __CSChatToUserDescribe__ : public SerializeDescribe
+{
+public:
+  __CSChatToUserDescribe__() { GetNameToObjectMap()["CSChatToUser"] = this; }
+  virtual ~__CSChatToUserDescribe__() {}
+
+protected:
+  virtual SerializeBase * CreateSerialize() const { return new CSChatToUser(); }
 };
 
 
